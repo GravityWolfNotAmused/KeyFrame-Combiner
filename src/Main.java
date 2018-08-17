773a911de2dfbb0c11e987af225225d52e214b2d
GravityWolfNotAmused <gravitywolfnotamused@gmail.com>
2018-08-17 15:37:10 -0400

    No need for that.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import imageConcat.ui.UIFrame;
 
 public class Main {
-    private static UIFrame ui;
     public static void main(String[] args) {
-        new Thread(() -> ui = new UIFrame()).run();
+        new Thread(() -> new UIFrame()).run();
     }
 }
